11fb6e3afdf7621b7a8bae67a738753f7f501859
hafizsidyck <hafizsidyck@gmail.com>
2024-06-05 08:43:09 +0200

    first commit

--- cm.js
+++ cm.js
@@ -1,31 +1,28 @@
-const nombreMystère = Math.floor(Math.random() * 100) + 1;
-btn = document.querySelector("button");
-p = document.querySelector("p");
-let guess = document.getElementById("guess");
-let Résultat = document.getElementById("Résultat");
-let input = document.querySelector("input"),
+const input = document.querySelector("input"),
+  p = document.querySelector("p"),
+  myst = document.querySelector("#myst"),
+  btn = document.querySelector("button");
+let randNumber = Math.floor(Math.random() * 100) + 1;
 
-
-//input.focus();
-
-console.log(nombreMystère);
-
+input.focus();
+console.log(randNumber);
 let i = 0;
 
 btn.addEventListener("click", checkInput);
-
-function Devinez() {
-  guess = parseInt(input.value);
-  input.value = "";
-  input.focus();
-  console.log(guess);
-  if (guess > nombreMystère) {
-    Résultat.textContent = "Trop grand";
-    i++;
-  } else if (guess < nombreMystère) {
-    Résultat.textContent = "Trop petit";
-  } else {
-    Résultat.textContent = "Bravo tu as trouvé au bout de ${i} tentatives, le nombre mystére était ${nombreMystère};";
-    input.disabled = true
-  }
+// Fonctions
+function checkInput(){
+    guess = parseInt(input.value);
+    input.value = "";
+    input.focus();
+    console.log(guess);
+    if (guess > randNumber) {
+      p.textContent = "Trop haut";
+      i++;
+    } else if (guess < randNumber) {
+      p.textContent = "Trop Bas";
+      i++;
+    } else {
+      p.textContent = Bravo tu as trouvé au bout de ${i} tentatives, le nombre mystére était ${randNumber};
+      input.disabled = true;
+    }
 }
